4a0c422fc640312590a6538aa2ac5261df08a70b
FHSWaller <nathan.waller@edu.forneyisd.net>
2021-10-26 14:48:40 +0000

    Finished the Circle Calc

---
+++ CircleCalc.java
@@ -0,0 +1,10 @@
+class CircleCalc{
+    public static void main(String[] args){
+        int radius = 3;
+        double circumference = 2 * Math.PI * radius;
+        double area = Math.PI * radius * radius;
+        System.out.println("The radius is : " + radius);
+        System.out.println("The circumference is : " + circumference);
+        System.out.println("The area is : " + area);
+    }
+}
