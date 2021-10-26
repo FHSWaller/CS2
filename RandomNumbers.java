8162d653d0c290b61eb627afe58e077bfd4a3c9f
FHSWaller <nathan.waller@edu.forneyisd.net>
2021-10-26 15:06:52 +0000

    Started Example

---
+++ RandomNumbers.java
@@ -0,0 +1,22 @@
+public class RandomNumbers {
+    public static void main(String[] args){
+        double num1 = Math.random();
+        //System.out.println(num1);
+        double num2 = Math.random();
+        double num3 = Math.random();
+        double num4 = Math.random();
+        double num5 = Math.random();
+
+        // Average Value
+
+
+        // Minimum value using Math.min()
+        
+
+
+        //Max value using Math.max()
+
+
+
+    }
+}
